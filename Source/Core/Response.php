e15ed11221fa3937ed44b5cc9c9d8b5c7c56feec
Nicko Gamba <p.nickogamba@gmail.com>
2022-01-26 11:38:34 +0800

    Redirect

---
+++ Source/Core/Response.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Waddup\Core;
+
+use Waddup\Exceptions\PageNotFound;
+
+class Response
+{
+    public static function redirect(string $location, int $code = 303)
+    {
+        header('Location:' . Request::getBaseURL()  . '/'. trim_slashes($location), true, $code);
+        exit;
+    }
+
+    /**
+     * @throws PageNotFound
+     */
+    public static function show404()
+    {
+        throw new PageNotFound('Page does not exist');
+    }
+}
